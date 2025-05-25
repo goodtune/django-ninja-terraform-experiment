8716e43bb94189fe1e149b0c225fddf49c163c78
Gary Reynolds <gary@touch.asn.au>
2025-05-25 20:31:24 +1000

    Implement list and detail views for Datacenter, Rack, Switch, and Server models

--- manifest/views.py
+++ manifest/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from manifest.models import Datacenter, Rack, Switch, Server
 
-# Create your views here.
+
+class DatacenterListView(ListView):
+    model = Datacenter
+    context_object_name = "datacenters"
+
+
+class DatacenterDetailView(DetailView):
+    model = Datacenter
+    context_object_name = "datacenter"
+
+
+class RackListView(ListView):
+    model = Rack
+    context_object_name = "racks"
+
+
+class RackDetailView(DetailView):
+    model = Rack
+    context_object_name = "rack"
+
+
+class SwitchListView(ListView):
+    model = Switch
+    context_object_name = "switches"
+
+
+class SwitchDetailView(DetailView):
+    model = Switch
+    context_object_name = "switch"
+
+
+class ServerListView(ListView):
+    model = Server
+    context_object_name = "servers"
+
+
+class ServerDetailView(DetailView):
+    model = Server
+    context_object_name = "server"
